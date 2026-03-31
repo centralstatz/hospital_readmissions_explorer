7686c4071cfb46ab3a767b9d56a7168317ea1d9e
zajichek <alexzajichek@gmail.com>
2026-03-31 08:00:27 -0500

    Changes in querychat API

--- global.R
+++ global.R
@@ -12,7 +12,7 @@ library(highcharter)
 library(leaflet)
 library(querychat)
 
-options(tigris_use_cache=TRUE)
+options(tigris_use_cache = TRUE)
 
 # Function to import dataset from CMS Provider Data Catalog (see https://github.com/zajichek/carecompare/blob/b1fa89382adfe77bd5f230f4162b03767ece10ea/R/FUNCTIONS.R#L99)
 pdc_read <-
@@ -20,26 +20,29 @@ pdc_read <-
     datasetid = NULL,
     ...
   ) {
-    
     # Check for input
-    if(is.null(datasetid)) 
+    if (is.null(datasetid)) {
       stop("Please specify a dataset identifier.")
-    
+    }
+
     # Make the url
-    url <- paste0("https://data.cms.gov/provider-data/api/1/metastore/schemas/dataset/items/", datasetid, "?show-reference-ids=false")
-    
+    url <- paste0(
+      "https://data.cms.gov/provider-data/api/1/metastore/schemas/dataset/items/",
+      datasetid,
+      "?show-reference-ids=false"
+    )
+
     # Make the request, extract the content
     request <- httr::content(httr::GET(url))
-    
+
     # Update the variable
     downloadurl <- request$distribution[[1]]$data$downloadURL
-    
+
     # Import the dataset
     readr::read_csv(
       file = downloadurl,
       ...
     )
-    
   }
 
 ## Import datasets
@@ -58,21 +61,21 @@ zips_wi <- tigris::zctas(year = 2010, state = "WI")
 # Make zip code centroids
 zips_wi_centroids <-
   zips_wi |>
-  
+
   # Get the centroid
   sf::st_centroid() |>
-  
+
   # Pluck the coordinates
   sf::st_coordinates() |>
-  
+
   # Make a tibble
   as_tibble() |>
-  
+
   # Add identifying column
   add_column(
     Zip = zips_wi$ZCTA5CE10
   ) |>
-  
+
   # Rename columns
   rename(
     lon = X,
@@ -82,13 +85,13 @@ zips_wi_centroids <-
 # Build map dataset
 hospitals <-
   hospitals |>
-  
+
   # Filter to Wisconsin hospitals
   filter(
     State == "WI",
     `Facility ID` %in% hrrp$`Facility ID`
   ) %>%
-  
+
   # Keep a few pieces of information
   select(
     FacilityID = `Facility ID`,
@@ -98,13 +101,13 @@ hospitals <-
     County = `County/Parish`,
     Zip = `ZIP Code`
   ) |>
-  
+
   # Join to get the centroid for the hospital's zip code
   inner_join(
     y = zips_wi_centroids,
     by = "Zip"
   ) |>
-  
+
   # Add random jitter to coordinates
   mutate(
     across(
@@ -116,39 +119,37 @@ hospitals <-
 # Make a clean measure lookup table
 hrrp <-
   hrrp |>
-  
+
   # Send down the rows
   pivot_longer(
     cols = matches("Readmission (Rate|Ratio)$"),
     names_to = "Measure",
     values_to = "Value"
   ) |>
-  
+
   # Keep/format select columns
   transmute(
     FacilityID = `Facility ID`,
-    DiagnosisCategory = 
-      `Measure Name` |>
-      
+    DiagnosisCategory = `Measure Name` |>
+
       # Remove prefix
       str_remove(pattern = "^READM-30-") |>
-      
+
       # Remove suffix
       str_remove(pattern = "-HRRP$"),
-    Measure =
-      Measure |>
-      
+    Measure = Measure |>
+
       # Remove suffix
       str_remove(pattern = "\\sReadmission (Rate|Ratio)$"),
     Value
   ) |>
-  
+
   # Filter to hospitals of interest; no missing measures
   filter(
     !is.na(Value),
     FacilityID %in% hospitals$FacilityID
   ) |>
-  
+
   # Send over the columns
   pivot_wider(
     names_from = Measure,
@@ -156,9 +157,9 @@ hrrp <-
   )
 
 # Make single table with all data
-master_dat <- 
+master_dat <-
   hospitals |>
-  
+
   # Join to get program results
   inner_join(
     y = hrrp,
@@ -177,26 +178,26 @@ state_outline <-
   )
 
 # County outlines
-county_outlines <- 
+county_outlines <-
   tigris::counties(cb = TRUE) %>%
   filter(
     STATE_NAME == "Wisconsin"
   )
 
 # Base map
-base_map <- 
+base_map <-
   leaflet() %>%
-  
+
   # Add geographic tiles
   addTiles() %>%
-  
+
   # Add WI state outline
   addPolygons(
     data = state_outline,
     fillColor = "gray",
     stroke = FALSE
   ) |>
-  
+
   # Add county outlines
   addPolygons(
     data = county_outlines,
@@ -205,28 +206,26 @@ base_map <-
     weight = 1,
     opacity = .5,
     fillOpacity = .35,
-    highlightOptions = 
-      highlightOptions(
-        color = "black",
-        weight = 3,
-        bringToFront = FALSE
-      ),
+    highlightOptions = highlightOptions(
+      color = "black",
+      weight = 3,
+      bringToFront = FALSE
+    ),
     label = ~NAME
   )
 
 # Set the pallette
-pal <- 
+pal <-
   colorNumeric(
     palette = "RdYlGn",
-    domain = -1*sort(unique(hrrp$Excess))
+    domain = -1 * sort(unique(hrrp$Excess))
   )
 
 # Configure the chat object
-querychat_config <- 
+querychat_config <-
   querychat_init(
-    df = master_dat,
-    tbl_name = "HospitalHRRP",
-    create_chat_func = purrr::partial(ellmer::chat_gemini),
+    data_source = querychat_data_source(master_dat, tbl_name = "HospitalHRRP"),
+    client = ellmer::chat_google_gemini,
     greeting = "Ask me a question about the HRRP in Wisconsin",
     data_description = readLines("data_description.md")
   )
